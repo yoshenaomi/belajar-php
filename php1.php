e83c71c769440a4d2019abdf5d5e7ae9bfecdc49
Yoshe Naomi Vici Aflaha <yoshenaomiviciaflaha@gmail.com>
2022-11-13 21:08:01 +0700

    php dasar 1'

---
+++ php1.php
@@ -0,0 +1,17 @@
+<?php
+
+    echo "Hello, World! <br>";
+
+    $nama = 'Yoshe Naomi Vici Aflaha';
+    $umur = 20;
+
+    echo "Nama saya <strong> $nama </strong>, dan usia saya sekarang $umur thn. <br>";
+
+    $namakakak = 'Vini Annesia Aflaha';
+    $umurkakak = 16;
+
+    echo "Nama kakak saya <strong> $namaadik </strong>, dan usia kakak saya sekarang $umurkakak thn. <br>";
+
+    $selisihumur = $umur % $umurkakak;
+    echo "Selisih umur saya dan kakak saya yaitu $selisihumur thn. <br>";
+?>
